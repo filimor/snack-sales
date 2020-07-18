5e02f323262f851c5a0c408f8d65a557f82439e1
Filipe Moreira <filimor@posteo.com.br>
2020-07-18 17:04:29 -0300

    Create the initial migration

--- SnackSales/SnackSales/Models/Category.cs
+++ SnackSales/SnackSales/Models/Category.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace SnackSales.Models
 {
     public class Category
     {
         public int Id { get; set; }
+        [StringLength(100)]
         public string Name { get; set; }
+        [StringLength(200)]
         public string Description { get; set; }
         public List<Snack> Snacks { get; set; }
     }
--- SnackSales/SnackSales/Models/Snack.cs
+++ SnackSales/SnackSales/Models/Snack.cs
@@ -1,13 +1,22 @@
-﻿namespace SnackSales.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SnackSales.Models
 {
     public class Snack
     {
         public int Id { get; set; }
+        [StringLength(100)]
         public string Name { get; set; }
+        [StringLength(100)]
         public string ShortDescription { get; set; }
+        [StringLength(255)]
         public string LongDescription { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
         public decimal Price { get; set; }
+        [StringLength(200)]
         public string ImageUrl { get; set; }
+        [StringLength(200)]
         public string ThumbnailUrl { get; set; }
         public bool Favorite { get; set; }
         public bool InStock { get; set; }
