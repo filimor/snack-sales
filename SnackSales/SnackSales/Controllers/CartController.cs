a418ec9f3e014879728cdf02244ee516c3a2f4b0
Filipe Moreira <filimor@posteo.com.br>
2020-07-20 19:14:09 -0300

    Implement Cart controller, view and view model

---
+++ SnackSales/SnackSales/Controllers/CartController.cs
@@ -0,0 +1,53 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using SnackSales.Models;
+using SnackSales.Repositories;
+using SnackSales.ViewModels;
+
+namespace SnackSales.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly Cart _cart;
+        private readonly ISnackRepository _snackRepository;
+
+        public CartController(ISnackRepository snackRepository, Cart cart)
+        {
+            _snackRepository = snackRepository;
+            _cart = cart;
+        }
+
+        public IActionResult Index()
+        {
+            _cart.CartItems = _cart.GetCartItems();
+            var cartViewModel = new CartViewModel
+            {
+                Cart = _cart,
+                TotalValue = _cart.GetTotalValue()
+            };
+            return View(cartViewModel);
+        }
+
+        public IActionResult AddToCart(int snackId)
+        {
+            var snack = _snackRepository.Snacks.FirstOrDefault(s => s.Id == snackId);
+            if (snack != null)
+            {
+                _cart.AddItem(snack, 1);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult RemoveFromCart(int snackId)
+        {
+            var snack = _snackRepository.Snacks.FirstOrDefault(s => s.Id == snackId);
+            if (snack != null)
+            {
+                _cart.RemoveItem(snack);
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ SnackSales/SnackSales/ViewModels/CartViewModel.cs
@@ -0,0 +1,10 @@
+﻿using SnackSales.Models;
+
+namespace SnackSales.ViewModels
+{
+    public class CartViewModel
+    {
+        public Cart Cart { get; set; }
+        public decimal TotalValue { get; set; }
+    }
+}
