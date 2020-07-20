bb7477a0897a9e787af976d8abaad70258679c43
Filipe Moreira <filimor@posteo.com.br>
2020-07-20 19:36:41 -0300

    Remove Privacy view

--- SnackSales/SnackSales/Controllers/HomeController.cs
+++ SnackSales/SnackSales/Controllers/HomeController.cs
@@ -23,11 +23,6 @@ namespace SnackSales.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
