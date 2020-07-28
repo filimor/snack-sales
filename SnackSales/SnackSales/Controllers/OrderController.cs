4bd228cf12aee65101b3cef1a0b754c340e57fce
Filipe Moreira <filimor@posteo.com.br>
2020-07-27 22:27:19 -0300

    Implement Order controller and Checkout view

---
+++ SnackSales/SnackSales/Controllers/OrderController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic;
+using SnackSales.Models;
+using SnackSales.Repositories;
+
+namespace SnackSales.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly Cart _cart;
+        private readonly OrderRepository _orderRepository;
+
+        public OrderController(OrderRepository orderRepository, Cart cart)
+        {
+            _orderRepository = orderRepository;
+            _cart = cart;
+        }
+
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Checkout(Order order)
+        {
+            _cart.CartItems = _cart.GetCartItems();
+
+            if (_cart.CartItems.Count == 0)
+            {
+                ModelState.AddModelError("","Seu carrinho está vazio.");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _orderRepository.CreateOrder(order);
+                TempData["Customer"] = order.Name;
+                TempData["OrderNumber"] = order.Id;
+                TempData["OrderDate"] = order.ShipmentDate;
+                TempData["TotalValue"] = _cart.GetTotalValue();
+                _cart.ClearCart();
+
+                return RedirectToAction("CompletedCheckout");
+            }
+
+            return View(order);
+        }
+
+        public IActionResult CompletedCheckout()
+        {
+            ViewBag.Customer = TempData["Customer"];
+            ViewBag.OrderNumber = TempData["OrderNumber"];
+            ViewBag.OrderDate = TempData["OrderDate"];
+            ViewBag.TotalValue = TempData["TotalValue"];
+            ViewBag.CompletedCheckoutMessage = "Obrigado pelo seu pedido :)";
+
+            return View();
+        }
+    }
+}
