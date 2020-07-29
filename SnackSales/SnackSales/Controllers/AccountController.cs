c826ccfb5167cbda14c3b8d647cd5ab3c20a2ad3
Filipe Moreira <filimor@posteo.com.br>
2020-07-29 17:31:24 -0300

    Implement Account controller

---
+++ SnackSales/SnackSales/Controllers/AccountController.cs
@@ -0,0 +1,54 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SnackSales.ViewModels;
+
+namespace SnackSales.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpGet]
+        public IActionResult Login(string returnUrl)
+        {
+            return View(new LoginViewModel
+            {
+                ReturnUrl = returnUrl
+            });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel loginViewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(loginViewModel);
+            }
+
+            var user = await _userManager.FindByNameAsync(loginViewModel.Username).ConfigureAwait(false);
+            if (user != null)
+            {
+                var result = await _signInManager.PasswordSignInAsync(user, loginViewModel.Password, false, false)
+                    .ConfigureAwait(false);
+                if (result.Succeeded)
+                {
+                    return string.IsNullOrEmpty(loginViewModel.ReturnUrl)
+                        ? RedirectToAction("Index", "Home")
+                        : RedirectToAction(loginViewModel.ReturnUrl);
+                }
+
+                ModelState.AddModelError("", "Usuário ou senha inválidos.");
+            }
+
+            return View(loginViewModel);
+        }
+    }
+}
---
+++ SnackSales/SnackSales/ViewModels/LoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SnackSales.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required] [Display(Name = "Usuário")] public string Username { get; set; }
+
+        [Required]
+        [Display(Name = "Senha")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public string ReturnUrl { get; set; }
+    }
+}
