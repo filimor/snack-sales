322c1d4e9885dea4f5673d6cb59abb44e005ce01
Filipe Moreira <filimor@posteo.com.br>
2020-07-30 17:58:36 -0300

    Create AddDeliveryDate migration

---
+++ SnackSales/SnackSales/Migrations/20200730205647_AddDeliveryDate.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SnackSales.Migrations
+{
+    public partial class AddDeliveryDate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeliveryDate",
+                table: "Orders",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DeliveryDate",
+                table: "Orders");
+        }
+    }
+}
